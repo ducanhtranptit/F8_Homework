9068b464ac7970b1c0460f12ccc17f409fa343b9
DucAnh <tda.ducanh@gmail.com>
2023-07-18 18:18:25 +0700

    add users

--- Buoi8/bai2.js
+++ Buoi8/bai2.js
@@ -57,6 +57,118 @@ const user2 = register(
   "user"
 );
 
+const user3 = register(
+  "Nguyen Van C",
+  "12345678",
+  "nguyenvanc@email.com",
+  "user"
+);
+
+const user4 = register(
+  "Nguyen Van D",
+  "123456789",
+  "nguyenvand@email.com",
+  "user"
+);
+
+const user5 = register(
+  "Nguyen Van E",
+  "1234567890",
+  "nguyenvane@email.com",
+  "user"
+);
+
+const user6 = register(
+  "Nguyen Van F",
+  "abcdefgh",
+  "nguyenvanf@email.com",
+  "user"
+);
+
+const user7 = register(
+  "Nguyen Van G",
+  "abcdefg123",
+  "nguyenvang@email.com",
+  "user"
+);
+
+const user8 = register(
+  "Nguyen Van H",
+  "654321",
+  "nguyenvanh@email.com",
+  "user"
+);
+
+const user9 = register(
+  "Nguyen Van I",
+  "qwerty",
+  "nguyenvani@email.com",
+  "user"
+);
+
+const user10 = register(
+  "Nguyen Van J",
+  "0987654321",
+  "nguyenvanj@email.com",
+  "user"
+);
+
+const user11 = register(
+  "Nguyen Van K",
+  "password",
+  "nguyenvank@email.com",
+  "user"
+);
+
+const user12 = register(
+  "Nguyen Van L",
+  "abcdef",
+  "nguyenvanl@email.com",
+  "user"
+);
+
+const user13 = register(
+  "Nguyen Van M",
+  "abc123",
+  "nguyenvanm@email.com",
+  "user"
+);
+
+const user14 = register(
+  "Nguyen Van N",
+  "123abc",
+  "nguyenvann@email.com",
+  "user"
+);
+
+const user15 = register(
+  "Nguyen Van O",
+  "test123",
+  "nguyenvano@email.com",
+  "user"
+);
+
+const user16 = register(
+  "Nguyen Van P",
+  "password123",
+  "nguyenvanp@email.com",
+  "user"
+);
+
+const user17 = register(
+  "Nguyen Van Q",
+  "123456abc",
+  "nguyenvanq@email.com",
+  "user"
+);
+
+const user18 = register(
+  "Nguyen Van R",
+  "abcdef123456",
+  "nguyenvanr@email.com",
+  "user"
+);
+
 // Đăng nhập
-const loggedInUser = login("nguyenvanb@email.com", "1234567", "hacker");
+const loggedInUser = login("nguyenvanb@email.com", "1234567", "user");
 console.log("dataLogin =", loggedInUser);
