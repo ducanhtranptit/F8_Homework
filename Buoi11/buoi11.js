980cd7243b322bd041a87f3e2ec1a48db5931d42
DucAnh <tda.ducanh@gmail.com>
2023-07-28 18:05:52 +0700

    Ơ

---
+++ Buoi11/buoi11.js
@@ -0,0 +1,21 @@
+function shotUrl(url) {
+  const apiUrl = ` https://api.shrtco.de/v2/shorten?url=${url}`;
+  return fetch(apiUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.ok) {
+        return data.result.full_short_link;
+      } else {
+        throw new Error(`ERROR: ${data.error}`);
+      }
+    })
+    .catch((error) => {
+      throw new Error(`ERROR: ${error.message}`);
+    });
+}
+
+const url = "https://www.facebook.com/ducanh.tran.927/";
+
+shotUrl(url)
+  .then((result) => console.log(`Shorten link: ${result}`))
+  .catch((error) => console.log(error.message));
