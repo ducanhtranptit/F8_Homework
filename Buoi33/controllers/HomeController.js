49a9973d6285416dca008a4323f3159778140092
User <user@MacBook-Pro-cua-User.local>
2023-10-17 21:41:13 +0700

    day33 completed

--- Buoi33/controllers/HomeController.js
+++ Buoi33/controllers/HomeController.js
@@ -1,17 +1,38 @@
 const model = require("../models/index");
 const User = model.User;
 const Link = model.Link;
-const shorturl = require("shorturl");
+const shortUrl = require("node-url-shortener");
 
 module.exports = {
-	index: (req, res, next) => {
-		res.render("index");
+	index: async (req, res) => {
+		const links = await Link.findAll({
+			where: {
+				user_id: req.user.id,
+			},
+		});
+		res.render("index", { links });
 	},
 	handleShortLink: async (req, res) => {
 		const { fullUrl } = req.body;
-		shorturl(fullUrl, (result) => {
-			console.log(result);
+		const user_id = req.user.id;
+		shortUrl.short(fullUrl, async (err, url) => {
+			const shortLink = await Link.findOne({
+				where: {
+					url: fullUrl,
+				},
+			});
+
+			if (!shortLink) {
+				const userLinkData = await Link.create({
+					user_id: user_id,
+					url: fullUrl,
+					shorturl: url,
+				});
+				console.log(userLinkData);
+				res.redirect("/");
+			} else {
+				res.redirect("/");
+			}
 		});
-		res.redirect("/");
 	},
 };
