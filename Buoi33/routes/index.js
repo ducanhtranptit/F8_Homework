e30b24049eac5e6d0f125da0a2e4827bcd89b822
User <user@MacBook-Pro-cua-User.local>
2023-10-18 15:32:38 +0700

    fix login  middleware

--- Buoi33/routes/index.js
+++ Buoi33/routes/index.js
@@ -2,9 +2,10 @@ var express = require("express");
 var router = express.Router();
 
 const Homecontroller = require("../controllers/HomeController");
+const LoginMiddleware = require("../middlewares/LoginMiddleware");
 
 /* GET home page. */
-router.get("/", Homecontroller.index);
+router.get("/", LoginMiddleware, Homecontroller.index);
 router.post("/", Homecontroller.handleShortLink);
 
 module.exports = router;
