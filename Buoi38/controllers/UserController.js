cd03cb3aa49d7ff67f40090f96c0d2f742cf7763
Bim béo <user@MacBook-Pro-cua-User.local>
2023-11-05 09:03:23 +0700

    day38

---
+++ Buoi38/controllers/UserController.js
@@ -0,0 +1,76 @@
+const cookie = require("cookie");
+const Redis = require("ioredis");
+
+const model = require("../models/index");
+const User = model.User;
+
+const redis = new Redis();
+
+class UserController {
+	async getUserById(req, res) {
+		const { id } = req.params;
+		const cookies = req.headers.cookie;
+
+		if (cookies) {
+			const parsedCookies = cookie.parse(cookies);
+
+			if (parsedCookies && parsedCookies.id === id) {
+				const userKey = `user:${id}`;
+				redis.get(userKey, (err, result) => {
+					if (err) {
+						return res.status(500).json({
+							status: "Error",
+							message: "Internal Server Error",
+						});
+					} else {
+						return res.status(200).json({
+							status: "Success",
+							data: result,
+						});
+					}
+				});
+			} else {
+				//lưu id vào cookie
+				const newCookie = cookie.serialize("id", id, {
+					maxAge: 3600,
+					httpOnly: true,
+					path: "/users",
+				});
+
+				res.setHeader("Set-Cookie", newCookie);
+
+				//Tìm xem user có tồn tại không
+				const user = await User.findByPk(id);
+				if (user) {
+					//Tồn tại thì thêm user đó vào cache
+					try {
+						const userKey = `user:${id}`;
+						const redisUser = await redis.hmset(userKey, user);
+						return res.status(200).json({
+							status: "Success",
+							userKey: typeof userKey,
+							redisUser,
+							data: user,
+						});
+					} catch (error) {
+						return res.status(500).json({
+							status: "Error",
+							message: "Internal Server Error",
+						});
+					}
+				} else {
+					return res.status(400).json({
+						status: "Error",
+						message: "User Invalid",
+					});
+				}
+			}
+		} else {
+			return res.status(401).json({
+				status: "Error",
+				message: "Unauthorized",
+			});
+		}
+	}
+}
+module.exports = new UserController();
---
+++ Buoi38/seeders/20231104144125-demo-user.js
@@ -0,0 +1,22 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		const data = [];
+
+		for (let index = 0; index < 50; index++) {
+			data.push({
+				name: `User ${index + 1}`,
+				email: `user${index + 1}@gmail.com`,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			});
+		}
+
+		await queryInterface.bulkInsert("users", data, {});
+	},
+
+	async down(queryInterface, Sequelize) {
+		await queryInterface.bulkDelete("Users", null, {});
+	},
+};
